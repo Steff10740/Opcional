b9aba0ec8a1e94970769ea05e412df36e0df8aa3
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 23:15:17 -0400

    Add files via upload

---
+++ TareaOpcional/TareaOpcional/TareaOpcional/Views/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaOpc.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaOpc.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+         async void Music01_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DetailPage(), true);
+        }
+    }
+}
