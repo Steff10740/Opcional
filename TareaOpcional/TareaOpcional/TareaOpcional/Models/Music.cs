5e8c8bf2848cc0a01b3d536a502bafe4b8536ec6
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 23:11:28 -0400

    Add files via upload

---
+++ TareaOpcional/TareaOpcional/TareaOpcional/Models/Music.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaOpc.Models
+{
+    public class Music
+    {
+        public string MusicPicture { get; set; }
+    }
+}
