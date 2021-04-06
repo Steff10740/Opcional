da46be680e64d462b2ece2483000cc5db8cf0534
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 23:18:27 -0400

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+
+namespace TareaOpc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.HomePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
