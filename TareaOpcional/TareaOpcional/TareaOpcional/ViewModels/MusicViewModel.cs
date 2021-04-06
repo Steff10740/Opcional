2e0316b8cb26dbc9e3b8cd6b9568407620cfb577
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-04-05 23:14:07 -0400

    Add files via upload

---
+++ TareaOpcional/TareaOpcional/TareaOpcional/ViewModels/MusicViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using TareaOpc.Models;
+
+namespace TareaOpc.ViewModels
+{
+     public class MusicViewModel
+    {
+        public ObservableCollection<Music> music { get; set; }
+
+        public MusicViewModel()
+        {
+            music = new ObservableCollection<Music>
+                {
+                    new Music { MusicPicture = "Music01"},
+                    new Music { MusicPicture = "Music02"},
+                    new Music { MusicPicture = "Music03"},
+                    new Music { MusicPicture = "Music04"},
+                    new Music { MusicPicture = "Music05"}
+
+                 };
+        }
+    }
+}
+    
+
+
